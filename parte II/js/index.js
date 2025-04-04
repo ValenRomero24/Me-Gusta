d9327a9cd3e6cf5365f3868b09977072af04f5d0
ValenRomero24 <valen0088@gmail.com>
2025-04-04 05:43:55 -0300

    Comentarios añadidos

--- parte II/js/index.js
+++ parte II/js/index.js
@@ -1,6 +1,10 @@
 let btnMeGusta = document.querySelectorAll('.btn-like');
-// console.log(btnMeGusta);
+// selecciono todos los botones de like (3)
 
+
+//Para cada uno de esos botones se le añade el evento onClick que busca el contador más cercano
+//para luego separarlo y tomar el primer elemento que es el numero de likes
+//despues incrementa dicho numero de likes y devuelve el nuevo valor al contador
 for(let i=0; i<btnMeGusta.length; i++){
     let contador = btnMeGusta[i].closest('.post-header').querySelector('.post-contador');
     btnMeGusta[i].addEventListener('click',function(evento){
